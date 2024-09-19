7aede388ab992a317960b223db2647454a30db36
a1inachen <92952629+a1inachen@users.noreply.github.com>
2024-09-18 20:58:19 -0400

    Visually appended post content preview to topic titles (#29)
    * append each topic title with content preview
    
    * addressed lint errors
    
    * removed console log from P1

--- src/categories/topics.js
+++ src/categories/topics.js
@@ -15,6 +15,20 @@ module.exports = function (Categories) {
 		let results = await plugins.hooks.fire('filter:category.topics.prepare', data);
 		const tids = await Categories.getTopicIds(results);
 		let topicsData = await topics.getTopicsByTids(tids, data.uid);
+		// Call getMainPosts to retreive actual post content (content not available in topics)
+		const mainPosts = await topics.getMainPosts(tids, data.uid);
+		// Remove HTML tags and get first 10 characteres of content to preview
+		const strippedContent = mainPosts.map((post) => {
+			const plainText = post.content.replace(/<[^>]*>/g, '').trim();
+			return plainText.substring(0, 10);
+		});
+		topicsData = topicsData.map((topic, index) => {
+			// Set titleRaw and title to stripped content corresp to same index
+			topic.titleRaw = `${topic.titleRaw}: ${strippedContent[index]}...`;
+			topic.title = `${topic.title}: ${strippedContent[index]}...`;
+			return topic;
+		});
+
 		topicsData = await user.blocks.filter(data.uid, topicsData);
 
 		if (!topicsData.length) {
