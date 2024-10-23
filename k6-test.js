c392e7a3950d4d0fddc0b7fdca970ac5594909b4
Alina Chen <alinache@andrew.cmu.edu>
2024-10-23 15:30:39 -0400

    fixed k6 modules

--- k6-test.js
+++ k6-test.js
@@ -1,6 +1,5 @@
 import http from 'k6/http';
-import { browser } from 'k6/browser';
-import { check, sleep } from 'k6';
+import { sleep } from 'k6';
 
 export const options = {
   // A number specifying the number of VUs to run concurrently.
