5013314fe0236ccd368bfbf98cddbe2e27f9ce76
Michaelli8899 <147345359+Michaelli8899@users.noreply.github.com>
2024-10-28 09:30:16 -0400

    test for failure

--- k6-test.js
+++ k6-test.js
@@ -1,63 +1,43 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import exec from 'k6/execution';
+import { check, sleep, fail } from 'k6';
+import { browser } from 'k6/browser';
 
 export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  // vus: 10,
-  // A string specifying the total duration of the test run.
-  // duration: '30s',
+  thresholds: {
+    http_req_failed: [
+      {
+        threshold: 'rate<0.00', 
+        abortOnFail: true,
+        delayAbortEval: '10s', // string
+      },
+    ],   // http errors should be less than 2%
+    http_req_duration: [
+      {
+        threshold: 'p(99) < 20',
+        abortOnFail: true,
+        delayAbortEval: '10s', // string
+      },
+    ], // 95% of requests should be below 2s
+    checks: ['rate>0.95'],
+  },
+  scenarios: {
+    performance: {
+      executor: 'ramping-vus',
+      stages : [
+        { duration: '1s', target: 10 }, 
+        { duration: '10s', target: 20 }, 
+        { duration: '10s', target: 40 }, 
+        { duration: '10s', target: 80 }, 
+        { duration: '10s', target: 160 }, 
+        { duration: '10s', target: 360 },
+        { duration: '10s', target: 720 },
+        { duration: '30s', target: 1440 }, 
+        { duration: '30s', target: 2880 }, 
+        { duration: '60s', target: 5760 }, 
 
-  // The following section contains configuration options for execution of this
-  // test script in Grafana Cloud.
-  //
-  // See https://grafana.com/docs/grafana-cloud/k6/get-started/run-cloud-tests-from-the-cli/
-  // to learn about authoring and running k6 test scripts in Grafana k6 Cloud.
-  //
-  // cloud: {
-  //   // The ID of the project to which the test is assigned in the k6 Cloud UI.
-  //   // By default tests are executed in default project.
-  //   projectID: 3720621,
-  //   // The name of the test in the k6 Cloud UI.
-  //   // Test runs with the same name will be grouped.
-  //   name: "test.js"
-  // },
-
-  // Uncomment this section to enable the use of Browser API in your tests.
-  //
-  // See https://grafana.com/docs/k6/latest/using-k6-browser/running-browser-tests/ to learn more
-  // about using Browser API in your test scripts.
-  //
-  // scenarios: {
-  //   // The scenario name appears in the result summary, tags, and so on.
-  //   // You can give the scenario any name, as long as each name in the script is unique.
-  //   ui: {
-  //     // Executor is a mandatory parameter for browser-based tests.
-  //     // Shared iterations in this case tells k6 to reuse VUs to execute iterations.
-  //     //
-  //     // See https://grafana.com/docs/k6/latest/using-k6/scenarios/executors/ for other executor types.
-  //     executor: 'constant-vus',
-  //     vus: 1,
-  //     duration: '10s',
-  //     options: {
-  //       browser: {
-  //         // This is a mandatory parameter that instructs k6 to launch and
-  //         // connect to a chromium-based browser, and use it to run UI-based
-  //         // tests.
-  //         type: 'chromium',
-  //       },
-  //     },
-  //   },
-  // },
-
-  stages : [
-    { duration: '30s', target: 100 }, 
-    { duration: '5s', target: 0 }, 
-    { duration: '30s', target: 100 }, 
-    { duration: '5s', target: 0 } 
-  ],
-  threshold: {
-    http_req_failed: ['rate<0.00'],   // http errors should be less than 2%
-    http_req_duration: ['p(95)<10'], // 95% of requests should be below 2s
+      ]
+    }
   }
 };
 
@@ -67,6 +47,11 @@ export const options = {
 // about authoring k6 scripts.
 //
 export default function() {
-  http.get('https://nodebb-team-bulbasaur1.azurewebsites.net');
+  // const res = http.get('https://nodebb-team-bulbasaur1.azurewebsites.net');
+  const res = http.get('https://test.k6.io');
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
   sleep(1);
-}
+};
+
