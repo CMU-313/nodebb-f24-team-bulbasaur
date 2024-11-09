fab238ae859a54e2d7179dc983fd2f17d5979ef7
Michaelli8899 <147345359+Michaelli8899@users.noreply.github.com>
2024-11-09 15:35:15 -0500

    fix lint

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,11 +1,11 @@
-var request = require('request');
+'use strict';
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    // Edit the translator URL below
-    const TRANSLATOR_API = "http://team-bulbasaur-translator-eec9ehgrcgaeasf6.canadacentral-01.azurewebsites.net/"
-    const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-    const data = await response.json();
-    return [data["is_english"], data["translated_content"]]
-}
+	// Edit the translator URL below
+	const TRANSLATOR_API = 'http://team-bulbasaur-translator-eec9ehgrcgaeasf6.canadacentral-01.azurewebsites.net/';
+	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
+	const data = await response.json();
+	return [data.is_english, data.translated_content];
+};
