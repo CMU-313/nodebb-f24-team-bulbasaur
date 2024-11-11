fb96be0ea06ecb7f939339390cc15deae0efea26
a1inachen <92952629+a1inachen@users.noreply.github.com>
2024-11-10 21:28:11 -0500

    updated translator api url

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,7 +4,7 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
 	// Edit the translator URL below
-	const TRANSLATOR_API = 'http://team-bulbasaur-translator-eec9ehgrcgaeasf6.canadacentral-01.azurewebsites.net/';
+	const TRANSLATOR_API = 'http://team-bulbasaur-translator-service.azurewebsites.net/';
 	const response = await fetch(`${TRANSLATOR_API}/?content=${postData.content}`);
 	const data = await response.json();
 	return [data.is_english, data.translated_content];
